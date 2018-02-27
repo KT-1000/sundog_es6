9e296e65aa055171f5baf04d02b48e8cba5d80a5
katie-simmons <katie@katie.codes>
2018-02-26 22:19:25 -0600

    List commands to load and query shakespeare

--- section-1/query_shakespeare.py
+++ section-1/query_shakespeare.py
@@ -1,2 +1,14 @@
-if __name__ = "__main__":
-    
+"""
+wget http://media.sundog-soft.com/es6/shakes-mapping.json
+curl -H 'Content-Type: application/json' -XPUT 127.0.0.1:9200/shakespeare --data-binary @shakes-mapping.json
+wget http://media.sundog-soft.com/es6/shakespeare_6.0.json
+curl -H 'Content-Type: application/json' -X POST 'localhost:9200/shakespeare/doc/_bulk?pretty' --data-binary  @shakespeare_6.0.json
+curl -H 'Content-Type: application/json' -XGET '127.0.0.1:9200/shakespeare/_search?pretty' -d '
+{
+    "query" : {
+        "match_phrase" : {
+            "text_entry" : "to be or not to be"
+        }
+    }
+}
+"""
